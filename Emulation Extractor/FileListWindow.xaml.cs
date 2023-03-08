4c0b000a44f4134484271e6e94c26a774ba5457c
alphapixel1 <alphapixel2@gmail.com>
2023-03-08 02:33:13 -0500

    worked on datagrid

--- Emulation Extractor/FileListWindow.xaml.cs
+++ Emulation Extractor/FileListWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Linq;
+using System.Collections;
+
 namespace Emulation_Extractor
 {
     /// <summary>
@@ -22,7 +24,7 @@ namespace Emulation_Extractor
         public List<GameFile> GameFiles;
         public FileListWindow(List<string> files)
         {
-            GameFiles=files.Select(e => new GameFile(e))/*.Where(e=>e.Emulator!=null || e.isZip)*/.ToList();
+            GameFiles=files.Select(e => new GameFile(e)).Where(e=>e.Emulator!=null || e.isZip).ToList();
             InitializeComponent();
         }
 
@@ -32,8 +34,30 @@ namespace Emulation_Extractor
             FileListView.ItemsSource = GameFiles;
             CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(FileListView.ItemsSource);
             PropertyGroupDescription groupDescription = new PropertyGroupDescription("EmulatorName");
+            //groupDescription.CustomSort =  new MyComparer();
             view.GroupDescriptions.Add(groupDescription);
             
         }
+      /*  public class MyComparer : IComparer<GameFile>,IComparer
+        {
+            public int Compare(GameFile x, GameFile y)
+            {
+                if (x.isZip)
+                    return -1;
+                else if (y.isZip)
+                    return 1;
+                else if (x.EmulatorName == "None")
+                    return 1;
+                else if (y.EmulatorName == "None")
+                    return -1;
+                return 0;
+            }
+
+            public int Compare(object? x, object? y)
+            {
+                //CollectionViewGroupInternal
+                return -1;
+            }
+        }*/
     }
 }
